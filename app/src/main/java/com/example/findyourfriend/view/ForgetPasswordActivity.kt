bfeaf9b88c60abb6c0a37a8f4b578b761e9d03ac
Anrtara Akter Zainab <antaraakterzainb@gmail.com>
2024-10-21 13:23:46 +0600

    updated

---
+++ app/src/main/java/com/example/findyourfriend/view/ForgetPasswordActivity.kt
@@ -0,0 +1,17 @@
+package com.example.findyourfriend.view
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.findyourfriend.R
+
+class ForgetPasswordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_forget_password)
+
+    }
+}
